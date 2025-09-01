f5cc9f511c3e656e5ee8910670f7514ecc727c63
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-09-01 11:59:45 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-// document.querySelector('.display')
-let n 
-function five(){
-    document.querySelector('.display').children[0].value=5
+let count = 0;
+let num1 = 0;
+let num2 = 0;
+let op 
+let display = document.querySelector('.display input');
+function numvalue(val) {
+    count++;
+    if (count <= 12) {
+        display.value += val;
+    }
+}
+
+function clearDisplay() {
+    count = 0;
+    display.value = '';
+}
+function symbo(va) {
+    num1 = Number(display.value)
+    clearDisplay()
+    op = va;
+    
+}
+function calculate() {
+    num2 = Number(display.value);
+    let result;
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num2 !== 0 ? num1 / num2 : 'Error';
+            break;
+        default:
+            result = 'Invalid';
+    }
+    display.value = 'Answer is : '+result;
+    count = 0;
 }
